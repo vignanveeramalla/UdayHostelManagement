b2d698ea71ce1d6b91854d445ae39ce8f1283553
Veeramalla Vignan <vignanveeramalla12@gmail.com>
2026-09-11 15:55:08 +0530

    Allow admin Vercel frontend in CORS

--- src/main/java/com/udayhostel/config/CorsConfig.java
+++ src/main/java/com/udayhostel/config/CorsConfig.java
@@ -19,7 +19,8 @@ public class CorsConfig
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://127.0.0.1:5501",
                 "http://localhost:5501",
-                "https://uday-boys-hostel.vercel.app"
+                "https://uday-boys-hostel.vercel.app",
+                "https://uday-boys-hostel-management.vercel.app"
         ));
 
         configuration.setAllowedMethods(Arrays.asList(
